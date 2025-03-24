f5db39e7c27abc84f5c99b7a9f773f4d8ee1bb50
QuoicouLC <bbalc.olivier@gmail.com>
2025-03-24 11:22:40 +0100

    Fuck le logo Vite (je ferais des nom de commits utile plus tard)

--- spring-boot-api/src/main/java/cartel/spring_boot_api/UserController.java
+++ spring-boot-api/src/main/java/cartel/spring_boot_api/UserController.java
@@ -1,6 +1,9 @@
 package cartel.spring_boot_api;
 
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -10,6 +13,14 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @PostConstruct
+    public void init() {
+        User defaultUser = new User();
+        defaultUser.setName("default");
+        defaultUser.setEmail("default@example.com");
+        userRepository.save(defaultUser);
+    }
+
     @PostMapping("/users")
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
@@ -17,10 +28,6 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
-        User defaultUser = new User();
-        defaultUser.setName("default");
-        defaultUser.setEmail("default@example.com");
-        userRepository.save(defaultUser);
         return userRepository.findAll();
     }
 
