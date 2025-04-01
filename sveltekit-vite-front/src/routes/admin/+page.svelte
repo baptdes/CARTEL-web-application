4f63a79deb9c4e9e3591487b09c6db09baaf3f62
Tom <tom.gutierrez1040@gmail.com>
2025-04-01 16:29:53 +0200

    page admin

---
+++ sveltekit-vite-front/src/routes/admin/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Admin from './admin.svelte';
+  import {nb2} from './page.js';
+</script>
+
+
+<main>
+  {#if $nb2 === 0}
+    <Admin/>
+
+  {:else if $nb2 === 1}
+    <h1>Catalogue</h1>
+    <button class="Thebouton" type="button" on:click={()=>$nb2=0}>Retour </button>
+  {:else if $nb2 === 2}
+    <h1>Course</h1>
+    <button class="Thebouton" type="button" on:click={()=>$nb2=0}>Retour </button>
+  {:else if $nb2 === 3}
+    <h1>Tresorerie</h1>
+    <button class="Thebouton" type="button" on:click={()=>$nb2=0}>Retour </button>
+  {:else if $nb2 === 4}
+    <h1>Empruntpret</h1>
+    <button class="Thebouton" type="button" on:click={()=>$nb2=0}>Retour </button>
+  {:else}
+    <h1>Erreur</h1>
+    <button class="Thebouton" type="button" on:click={()=>$nb2=0}>Retour </button>
+  {/if}
+</main>
+
+<style>
+</style>
---
+++ sveltekit-vite-front/src/routes/admin/admin.svelte
@@ -0,0 +1,89 @@
+<script>
+  import Counter from './lib/Counter.svelte';
+  import {nb2} from './page.js';
+</script>
+
+
+<main>
+  <div class="boxes">
+    <div class="ext">
+      <div class="minbox">
+        <div class="Catalogue bouton">
+          <button class="Thebouton" type="button" on:click={()=>$nb2=1}><center>Catalogue</center></button>
+        </div>
+      </div>
+      <div class="minbox">
+        <div class="Course bouton">
+          <button class="Thebouton" type="button" on:click={()=>$nb2=2}>Purger Course </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="ext">
+      <div class="minbox">
+        <div class="Tresorerie bouton">
+          <button class="Thebouton" type="button" on:click={()=>$nb2=3}>Trésorerie</button>
+        </div>
+      </div>
+      <div class="minbox">
+        <div class="Empruntpret bouton">
+          <button class="Thebouton" type="button" on:click={()=>$nb2=4}>Emprunt/prêt </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<style>
+    .Catalogue{
+        background: white;
+        height:10em;
+        width:40em;
+    }
+
+    .Course{
+      background: white;
+      height:10em;
+      width:40em;
+    }
+
+    .Tresorerie{
+      background: white;
+      height:10em;
+      width:40em;
+    }
+
+    .Empruntpret{
+      background: white;
+      height:10em;
+      width:40em;
+    }
+
+    .bouton{
+      border-radius: 0.5em;
+      display: block;
+      padding:0em;
+    }
+
+    .Thebouton{
+      background: rgba(255,255,255,0);
+      color :red;
+      font-size: 2em;
+      height:10em;
+      width:40em;
+    }
+
+    .minbox{
+      padding: 1em;
+    }
+
+    .ext{
+      padding: 0em;
+    }
+    
+    .boxes{
+      padding: 0em;
+      display: flex;
+    }
+ 
+</style>
