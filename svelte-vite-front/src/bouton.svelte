22d39a52bc97bf8735b0b30e4362c39fbd67be4a
Tom <tom.gutierrez1040@gmail.com>
2025-04-01 15:31:27 +0200

    page dynamic

---
+++ svelte-vite-front/src/bouton.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let name;
+    export let img='./assets/LEGO_logo.png';
+	
+</script>
+
+<div class="Thebackground" style="background-image: url(${img});">
+    <button class="Thebouton" type="button">{name}</button>
+</div>
+
+
+<style>
+    .Thebackground{
+        display: block;
+    }
+
+    .Thebouton{
+        background: rgba(255,255,255,0);
+        
+    }
+</style>
