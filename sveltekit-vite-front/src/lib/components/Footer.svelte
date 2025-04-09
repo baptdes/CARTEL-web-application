5c63c67153d480c2819c8369f67117f15c375f8c
baptdes <desnouck.baptiste@gmail.com>
2025-04-09 21:55:04 +0200

    Fix social media text

--- sveltekit-vite-front/src/lib/components/Footer.svelte
+++ sveltekit-vite-front/src/lib/components/Footer.svelte
@@ -21,7 +21,7 @@
         </a>
         <a href="https://jejeu.org/jeu.php?id=29" target="_blank" rel="noopener noreferrer" class="social-link">
             <img src="/icons/adult-18.svg" alt="Adulte +18 Logo" class="icon" />
-          <span>Adulte +18</span>
+          <span>OnlyFan</span>
         </a>
       </div>
     </div>
